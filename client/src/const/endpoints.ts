ae73d66291643eef3dd4868d898327da0586dc19
Operkod <vip.operkod5@gmail.com>
2021-09-05 23:33:48 +0300

    build

--- client/src/const/endpoints.ts
+++ client/src/const/endpoints.ts
@@ -1,6 +1,6 @@
 import getParams from 'helpers/getParams';
 
-const host = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+const host = window.location.origin; // TODO:
 const signIn = 'auth/signin';
 const signUp = 'auth/signup';
 const user = 'auth/user';
